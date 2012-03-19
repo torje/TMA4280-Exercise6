3dbd7ec3aa579b37e3e0d83c24ba12e0cb849314
Torje Hoås Digernes <torje@bialetti>
2012-03-19 15:03:02 +0000

    foreldet transposen min;

--- transpose.h
+++ transpose.h
@@ -1,15 +1,6 @@
-#include <stdlib.h>
-typedef double real;
-struct matrix_st;
-	int dim;
-	real ** i_index;
-	real * j_index;
-};
-typedef struct matrix_st matrix_t;
-void MATRIX_MPI_transpose( matrix_t matrix, MPI_Comm comm, int procs, int rank);
-void MPI_transpose(matrix_t matrix);
-void REAL_xor_swap(real * a, real * b );
-int twinrank(int dim, int rank);
-matrix_t * MATRIX_dup(matrix_t *orig);
-void MATRIX_arraycopy(matrix_t orig, matrix_t);
-void MATRIX_infocopy(matrix_t * orig, matrix_t * copy);
+typedef double real;	
+typedef struct {
+	real **rows;
+	real *cols;
+}; matrix;
+
